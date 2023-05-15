6dc00ab157db9d95dd76aaf3ce005549715aa2fd
murf <murf@the1977project.org>
2023-05-15 10:30:39 +0100

    Starting

---
+++ projects.py
@@ -0,0 +1,28 @@
+# The Project File
+# (C) Damian Murphy. Original Projects.txt, 1995/1996-2003, previous organisers 1989-1993
+#
+
+import json
+import io
+
+""" Data
+    Project:
+        Number: n
+        Idea: String
+        Added: Date
+        Done: Date
+        Started On: Date # Implies in progress
+        Stopped On: Date
+        Continuous: Neverending idea
+        Links: String
+        Memoranda: Large notes section
+        Log: Start/Stop log?
+"""
+
+def start():
+    """ This bit is the main control """
+
+
+# Start me up
+if __name__ == '__main__':
+    start()
