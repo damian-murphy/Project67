d342953c13058840fd7bc76880757584413b0386
murf <murf@the1977project.org>
2023-06-13 00:32:13 +0100

    Editing form more or less working. Style updated for forms a bit. More to do.

--- projects.py
+++ projects.py
@@ -38,13 +38,17 @@ def start():
 
     @app.route("/")
     def home():
-        return render_template("index.html.j2", title="Welcome")
+        """ Main index page, also show a list of currently active projects for focus """
+        db = database.get_db()
+        projects = db.execute("""select number,idea,created,started_on from projects 
+                        where started_on is not NULL and done='0' order by started_on""").fetchall()
+        return render_template("index.html.j2", title="Welcome", projects=projects)
 
     @app.route("/list")
     def list():
         db = database.get_db()
         projects = db.execute("select number,idea,created,done from projects order by number").fetchall()
-        return render_template("list.html.j2", projects=projects)
+        return render_template("list.html.j2", title="List", projects=projects)
 
     @app.route("/project/<num>")
     def show_project(num):
@@ -52,7 +56,7 @@ def start():
         project = db.execute("""select number,idea,created,done,memoranda,last_modified,
                              started_on,stopped_on,continuous,links
                              from projects where number = ?""", (num, )).fetchone()
-        return render_template("project.html.j2", project=project)
+        return render_template("project.html.j2", title=project['idea'], project=project)
 
     @app.route("/project/<num>/edit", methods=("GET", "POST"))
     def edit_project(num):
@@ -62,8 +66,9 @@ def start():
                                  from projects where number = ?""", (num,)).fetchone()
 
         if request.method == "POST":
-            idea = request.form["Idea"]
+            idea = request.form["idea"]
             memoranda = request.form["memoranda"]
+            created =
             error = None
 
             if not idea:
@@ -72,15 +77,16 @@ def start():
             if error is not None:
                 flash(error)
             else:
-                db = get_db()
+                db = database.get_db()
                 db.execute(
-                    "INSERT INTO post (idea, memoranda) VALUES (?, ?)",
-                    (idea, memoranda),
+                    """UPDATE projects SET idea = ?, memoranda = ?, created = ?, done = ?, last_modified = ?,
+                    started_on = ?, stopped_on = ?, continuous = ?, links = ? where number = ?;""",
+                    (idea, memoranda, created, num)
                 )
                 db.commit()
-                return redirect(url_for("blog.index"))
+                return redirect(url_for("show_project", num=num))
 
-        return render_template("edit.html.j2", project=project)
+        return render_template("edit.html.j2", title="Editing", project=project)
 
     return app
 
