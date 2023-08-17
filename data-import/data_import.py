03c1cf9fd643341d8966ed55342ec52e81f00e1f
damian.murphy <damian.murphy@ward.ie>
2023-08-17 17:03:20 +0100

    trying to parse for null values and dealing with NaN and it's not working.

--- data-import/data_import.py
+++ data-import/data_import.py
@@ -3,12 +3,19 @@
 # Initialises a Sqlite3 db, imports a csv, inserts into the db
 # simples
 # (C)2023 DJM LZP
-
+import datetime
 import pandas as pd
 import sqlite3
 
 CSVFILE = "projects.csv"
 SCHEMA = "db_init.sql"
+
+def dt_from_str(string):
+    """ Simple func to return datetime based on string input """
+    if string is not None:
+        return datetime.datetime.strptime(str(string), "%d/%m/%Y %H:%M")
+    else:
+        return None
 
 if __name__ == '__main__':
     csvdata = pd.read_csv(CSVFILE)
@@ -22,9 +29,26 @@ if __name__ == '__main__':
 
     # Load the data using executemany
     # It's in a dataframe, and we can discard the dataframe index as we don't need it
-    database.executemany(
-        "INSERT INTO projects VALUES(:number, :idea, :created, :done, :started_on, :stopped_on, :continuous, :links, :memoranda, :last_modified)",
-        csvdata.itertuples(index=False))
+    # database.executemany(
+    #     "INSERT INTO projects VALUES(:number, :idea, :created, :done, :started_on, :stopped_on, :continuous, :links, :memoranda, :last_modified)",
+    #     csvdata.itertuples(index=False))
+    for row in csvdata.itertuples(index=False):
+
+        f_created = dt_from_str(row[2])
+        f_done = dt_from_str(row[3])
+        f_started_on = dt_from_str(row[4])
+        f_stopped_on = dt_from_str(row[5])
+        f_last_modified = dt_from_str(row[9])
+
+        rowdata = (
+            {"number": row[0], "idea": row[1], "created": f_created, "done": f_done,
+             "started_on": f_started_on, "stopped_on": f_stopped_on, "continuous": row[6],
+             "links": row[7], "memoranda": row[8], "last_modified": f_last_modified
+            }
+        )
+
+        database.execute("INSERT INTO projects VALUES(:number, :idea, :created, :done, :started_on, :stopped_on, :continuous, :links, :memoranda, :last_modified)",
+                         rowdata)
     database.commit()
 
     database.close()
